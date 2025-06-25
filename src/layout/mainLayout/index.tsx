6afb9b19bb72c6a43695849863eb7382a24c09bc
NamitInfeon <namit.khobragde@infeonit.in>
2025-06-25 12:38:09 +0530

    corousel added

--- src/layout/mainLayout/index.tsx
+++ src/layout/mainLayout/index.tsx
@@ -2,14 +2,19 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../../components/common/Navbar";
 import Footer from "../../components/common/Footer";
+import Corousel from "../../components/common/Corousel";
+import About from "../../pages/About";
 
 const MainLayout = () => {
   return (
     <div className="main-layout">
       <Navbar />
+      <Corousel/>
+
       <main>
         <Outlet />
       </main>
+      <About />
       <Footer />
     </div>
   );
